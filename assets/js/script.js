152a6e7ce2b0ace3994016e4819dd1f92132ccee
kpegeder <k.pegeder@gmail.com>
2020-08-07 11:32:49 -0700

    update code to display all questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const questionList = [
   {
     question: "What tag is used for JavaScript?",
     multipleChoice: ["<head>", "<body>", "<script>", "<button>"],
-    answer: "2"
+    answer: 2
   },
   {
     question: 'What syntax is used to refer to an external code, "hello.js"?',
@@ -12,39 +12,39 @@ const questionList = [
       '<script hreg="hello.js">',
       "None of the above"
     ],
-    answer: "0"
+    answer: 0
   },
   {
     question: "Var is used to define which data types?",
     multipleChoice: ["String", "Numbers", "Booleans", "All of the above"],
-    answer: "3"
+    answer: 3
   },
   {
     question: "Prompt can be used to get true or false reponse?",
     multipleChoice: ["True", "False"],
-    answer: "0"
+    answer: 0
   },
   {
     question:
       "The ______ method displays an alert box with a specified message and an OK button.",
     multipleChoice: ["confirm()", "alert()", "prompt()", "None of the above"],
-    answer: "1"
+    answer: 1
   },
   {
     question: "JavaScript can be use to change the html code?",
     multipleChoice: ["True", "False"],
-    answer: "0"
+    answer: 0
   },
   {
     question:
       "The setInterval() method calls a function or evaluates an expression at specified intervals measure in ______ .",
     multipleChoice: ["seconds", "minutes", "milliseconds", "None of the above"],
-    answer: "2"
+    answer: 2
   },
   {
     question: "A loop will continue until the condition is ",
     multipleChoice: ["True", "False"],
-    answer: "1"
+    answer: 1
   },
   {
     question: "Methods to navigate teh DOM",
@@ -54,12 +54,12 @@ const questionList = [
       "nextSibling",
       "All of the above"
     ],
-    answer: "3"
+    answer: 3
   },
   {
     question: "A condition is wrapped by ",
     multipleChoice: ["brackets", "parentheses", "curly brackets"],
-    answer: "1"
+    answer: 1
   }
 ];
 
@@ -76,93 +76,96 @@ const questionContainer = document.querySelector(".question-container");
 
 // let secondsLeft = 10;
 let correct = 0;
-let questionCount = 0;
-let possibleQuestion = [];
+let count = 0;
+let availableQuestion = [];
 let currentQuestion = 0;
-// let
-
-// // Timer Count Down
-// function setTime() {
-//   var timerInterval = setInterval(function () {
-//     secondsLeft--;
-//     timeEl.textContent = secondsLeft;
-
-//     if (secondsLeft === 0) {
-//       clearInterval(timerInterval);
-//     }
-//   }, 1000);
-// }
-// setTime();
+let answerNumber = [];
+let score;
 
 startQuiz.addEventListener("click", beginQuiz);
-// i++;
 
-// question.addEventListener("click", displayQs());
-
-function availableQuestion() {
-  const totalQuestion = questionList.length;
-  for (let i = 0; i < totalQuestion; i++) {
-    possibleQuestion.push(questionList[i]);
-  }
+// Display Intro to Quiz
+function beginQuiz() {
+  console.log("Start Quiz");
+  introQuiz.innerHTML = "";
+  setQuestion();
 }
 
-function displayQuestion() {
+possibleAnswer.addEventListener("click", setQuestion);
+
+// Display question
+function setQuestion() {
+  // Check status of question
+  if (count == questionList.length) {
+    endQuiz();
+    return;
+  }
+  // Display Question Number
   questionNumber.textContent =
-    "Question " + (questionCount + 1) + " of " + questionList.length;
-  question.textContent = questionList[currentQuestion].question;
-  possibleAnswer.innerHTML = "";
-  questionList[currentQuestion].multipleChoice.forEach((multipleChoice) => {
-    var button = document.createElement("button");
-    var list = document.createElement("li");
-    button.textContent = multipleChoice;
+    "Question " + (count + 1) + " of " + questionList.length;
+  // Display Questions and Answers
+  question.textContent = questionList[count].question;
+  console.log("Display Question " + (count + 1));
+  // Clear the list of answers
+  possibleAnswer.textContent = "";
+  // Create List and Button
+  for (let j = 0; j < questionList[count].multipleChoice.length; j++) {
+    let listMultiChoice = document.createElement("li");
+    let btnMultiChoice = document.createElement("button");
+    btnMultiChoice.textContent = questionList[count].multipleChoice[j];
 
-    possibleAnswer.appendChild(list);
-    list.appendChild(button);
-  });
+    btnMultiChoice.addEventListener("click", checkAnwser);
+    possibleAnswer.append(listMultiChoice);
+    listMultiChoice.append(btnMultiChoice);
+  }
 
+  // console.log("Count = " + count);
+  // console.log("Correct " + correct);
+}
+
+function checkAnwser() {
   let element = event.target;
+  console.log("Select Answer " + element.textContent);
+  console.log(
+    "Answer " + questionList[count].multipleChoice[questionList[count].answer]
+  );
   if (
     element.textContent ===
-    questionList[currentQuestion].multipleChoice[
-      questionList[currentQuestion].answer
-    ]
+    questionList[count].multipleChoice[questionList[count].answer]
   ) {
     correct++;
-    console.log(correct);
+    console.log("Correct " + correct);
   }
-  console.log(currentQuestion);
-  currentQuestion++;
-  questionCount++;
-  if (questionCount == questionList.length) {
-    endQuiz();
-  }
+  // Create and else with a penalty
+
+  console.log("Current " + count);
+
+  count++;
 }
 
-// Check if answer is correct
-// possibleAnswer.button.addEventListener("click",
-// function (event) {
-//
-//   if (
-//     element.textContent ===
-//     questionList[i].multipleChoice[questionList[i].answer]
-//   ) {
-//     correct++;
-//     console.log(correct);
+// Get all the answer values
+// function answerList() {
+//   const totalQuestion = questionList.length;
+//   for (let i = 0; i < totalQuestion; i++) {
+//     answerNumber.push(questionList[i].answer);
+//     availableQuestion.push(questionList[i].question);
 //   }
-//   console.log(i);
-//   i++;
-// });
-possibleAnswer.addEventListener("click", displayQuestion);
-
-function beginQuiz() {
-  introQuiz.style.display = "none";
-
-  displayQuestion();
-}
+//   console.log(answerNumber, availableQuestion);
+// }
+// answerList();
 
 function endQuiz() {
-  questionContainer.style.display = "none";
+  questionContainer.innerHTML = "";
+  // Page set up
 }
-// function highScore() {}
 
-availableQuestion();
+// function recordScore() {
+//   // Check localStorage for users
+//   let user = {
+//     user: user,
+//     score: correct
+//   };
+//   localStorage.setItem("user", JSON.stringify(user));
+// }
+
+// function highScore() {}
